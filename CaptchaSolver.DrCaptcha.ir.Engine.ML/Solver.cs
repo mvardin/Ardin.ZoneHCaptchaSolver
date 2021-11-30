a77c44173facfadce090d908ad5243f63b867192
Ardin <Ardin@gmail.com>
2021-11-30 12:10:43 +0330

    Add ML Engine

---
+++ CaptchaSolver.DrCaptcha.ir.Engine.ML/Solver.cs
@@ -0,0 +1,45 @@
+﻿using DrCaptcha_irML.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptchaSolver.DrCaptcha.ir.Engine.ML
+{
+    public static class Solver
+    {
+        public static string Solve(string captchaPath)
+        {
+            try
+            {
+                var filtered = MethodsImageFilter.CopyBitmap(new Bitmap(captchaPath));
+                filtered = MethodsImageFilter.RemoveAllColorsAndPlaceOnTheFinalImage(filtered);
+                filtered = MethodsImageFilter.Closing(filtered);
+                filtered = MethodsImageFilter.Invert(filtered);
+                var blobs = MethodsImageFilter.GetBlob(filtered);
+
+                string code = string.Empty;
+                for (int i = 0; i < blobs.Length; i++)
+                {
+                    string filename = Guid.NewGuid() + ".png";
+                    string imgSource = "c:\\temp\\" + filename;
+                    blobs[i].Save(imgSource);
+                    ModelInput sampleData = new ModelInput()
+                    {
+                        ImageSource = imgSource
+                    };
+
+                    var predictionResult = ConsumeModel.Predict(sampleData);
+                    code += predictionResult.Prediction;
+                }
+                return code;
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ CaptchaSolver.DrCaptcha.ir.Engine.ML/ZoneH.cs
@@ -0,0 +1,40 @@
+﻿using DrCaptcha_irML.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptchaSolver.DrCaptcha.ir.Engine.ML
+{
+    public static class ZoneH
+    {
+        public static string Solve(string captchaFilePath)
+        {
+            var filtered = MethodsImageFilter.CopyBitmap(new Bitmap(captchaFilePath));
+            filtered = MethodsImageFilter.RemoveAllColorsAndPlaceOnTheFinalImage(filtered);
+            filtered = MethodsImageFilter.Closing(filtered);
+            filtered = MethodsImageFilter.Invert(filtered);
+            var blobs = MethodsImageFilter.GetBlob(filtered);
+
+            string code = string.Empty;
+            for (int i = 0; i < blobs.Length; i++)
+            {
+                string filename = Guid.NewGuid() + ".png";
+                string imgSource = "c:\\temp\\" + filename;
+                blobs[i].Save(imgSource);
+                ModelInput sampleData = new ModelInput()
+                {
+                    ImageSource = imgSource
+                };
+
+                var predictionResult = ConsumeModel.Predict(sampleData);
+                code += predictionResult.Prediction;
+                File.Delete(imgSource);
+            }
+            return code;
+        }
+    }
+}
